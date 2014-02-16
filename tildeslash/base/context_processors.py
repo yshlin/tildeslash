629f9bbc28c5d4b6a8dd8dc1419c9d60a0d6e63e
Eddie Lin <yshlin@gmail.com>
2014-02-16 21:15:12 +0800

    Added welcome post.

--- tildeslash/base/context_processors.py
+++ tildeslash/base/context_processors.py
@@ -2,4 +2,4 @@ from tildeslash import settings
 
 
 def setting_values(request):
-    return {'CODELOG_TITLE': settings.CODELOG_TITLE}
+    return {'CODELOG_TITLE': settings.CODELOG_TITLE if settings.CODELOG_TITLE else 'tildeslash codelog'}
